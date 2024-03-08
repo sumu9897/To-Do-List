24401fad2a3faaa0e03552309212695da0c5d9e7
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-03-08 22:45:23 +0600

    To-Do-List

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ const helmet=require('helmet');
 const hpp=require('hpp');
 const cors=require('cors');
 const mongoose=require('mongoose');
+
+
+//Cors Open
+app.use(cors());
+
+//Security Package Implementation
+app.use(helmet())
+app.use(hpp())
+app.use(express.json({limit:'20mb'}))
+app.use(express.urlencoded({extended:true}));
+
+let limiter=rateLimit({windowMs:15*60*1000,max:3000})
+app.use(limiter);
+
+let URL="mongodb://localhost:27017/Todo"
+let OPTION={user:"",pass:"",autoIndex:true}
+
+mongoose.connect(URL,OPTION).then((res)=>{
+    console.log(("Database Connceted"))
+}).catch((err)=> {
+    console.log(err)
+})
+
+
+//Route Implement
+app.use("/api",router);
+
+app.use("*",(req,res)=>{
+    res.status(404).json({data:"Not found"})
+})
+
+module.exports=app;
---
+++ index.js
@@ -0,0 +1,4 @@
+const app=require('./app')
+app.listen(5080,function () {
+    console.log("Running.....@5050");
+})
